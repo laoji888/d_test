8a605046ec65b006eb61b3dbdae8ee8ecf8f6e97
laoji888 <xztlaoji@163.com>
2020-01-14 20:57:36 +0800

    完善数据库信息

--- t1/models.py
+++ t1/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Grades(models.Model):
+    gname = models.CharField(max_length=20)
+    adate = models.DateTimeField()
+    ggirlnum = models.IntegerField()
+    gboynum = models.IntegerField()
+    isdelete = models.BooleanField(default=False)
+
+
+class students(models.Model):
+    sname = models.CharField(max_length=20)
+    sgender = models.BooleanField()
+    sage = models.IntegerField()
+    scondent = models.CharField(max_length=20)
+    isdelete = models.BooleanField(default=False)
+    sgrade = models.ForeignKey("Grades")
+
+
